b9fe001d87711c8d8ffb8713f90d099ccdfde800
Roselia021143 <axiosxxi@gmail.com>
2026-06-19 10:03:53 +0700

    ๊Update

--- add_email_visibility.sql
+++
@@ -1,5 +0,0 @@
-USE alumni_db;
-
-ALTER TABLE students
-    ADD COLUMN IF NOT EXISTS email_visible TINYINT(1) NOT NULL DEFAULT 0
-    AFTER phone_visible;
--- add_student_user_email.sql
+++
@@ -1,7 +0,0 @@
-USE alumni_db;
-
-ALTER TABLE student_users
-    ADD COLUMN IF NOT EXISTS email VARCHAR(150) NULL AFTER username;
-
-CREATE UNIQUE INDEX IF NOT EXISTS uq_student_users_email
-    ON student_users (email);
